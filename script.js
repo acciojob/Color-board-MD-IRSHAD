926e245d0e9fb13b2574327e05914d85dbb704fe
MD-IRSHAD <114646710+MD-IRSHAD@users.noreply.github.com>
2023-08-01 17:35:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+  const colorBoard = document.getElementById("colorBoard");
+
+  // Function to generate boxes and add them to the board
+  function generateBoxes() {
+    const totalBoxes = 800;
+
+    for (let i = 0; i < totalBoxes; i++) {
+      const box = document.createElement("div");
+      box.className = "box";
+      colorBoard.appendChild(box);
+    }
+  }
+
+  generateBoxes();
+});
