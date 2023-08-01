f61818df9e4e5f2e4529996bf084a8b56f9667f8
MD-IRSHAD <114646710+MD-IRSHAD@users.noreply.github.com>
2023-08-01 17:39:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 //your JS code here. If required.
-document.addEventListener("DOMContentLoaded", function () {
-  const colorBoard = document.getElementById("colorBoard");
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
 
-  // Function to generate boxes and add them to the board
-  function generateBoxes() {
-    const totalBoxes = 800;
 
-    for (let i = 0; i < totalBoxes; i++) {
-      const box = document.createElement("div");
-      box.className = "box";
-      colorBoard.appendChild(box);
-    }
-  }
 
-  generateBoxes();
-});
+const container  = document.getElementById("container")
+
+const SQUARES = 800
+
+for(let i=0;i<SQUARES;i++){
+    const square = document.createElement("div")
+    square.classList.add("square")
+
+    square.addEventListener("mouseover",()=> setColor(square))
+    square.addEventListener("mouseout",()=> removeColor(square))
+
+    container.appendChild(square)
+}
+
+function setColor(element){
+    const color = randomColor()
+
+    element.style.background = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+
+}
+
+function removeColor(element){
+    element.style.background = "#1d1d1d"
+    element.style.boxShadow = "0 0 2px #000"
+}
+function randomColor(){
+    const index = Math.floor(Math.random()*colors.length)
+    return colors[index]
+}
